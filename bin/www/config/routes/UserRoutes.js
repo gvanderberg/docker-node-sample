e6f5c81b3c6490d4948f607d8ce77da8c59505f7
Gareth van der Berg <gareth.shaun@gmail.com>
2016-04-30 18:01:32 +0200

    reallign users views and routes

--- bin/www/config/routes/UserRoutes.js
+++ bin/www/config/routes/UserRoutes.js
@@ -1,18 +1,23 @@
 "use strict";
 const express = require("express");
-const UserController = require("./../../app/controllers/UserController");
+const UserRepository = require("./../../app/repositories/UserRepository");
 let router = express.Router();
 class UserRoutes {
     constructor() {
-        this._userController = new UserController();
+        this._userRepository = new UserRepository();
     }
     get routes() {
-        let controller = this._userController;
-        router.delete("/:_id", controller.delete);
-        router.get("/", controller.retrieve);
-        router.get("/:_id", controller.findById);
-        router.post("/", controller.create);
-        router.put("/:_id", controller.update);
+        let repository = this._userRepository;
+        router.get("/", (req, res) => {
+            console.log("Got a GET request for the Users page.");
+            repository.retrieve((error, result) => {
+                res.render("users/list", { "title": "All My Ninjas", "users": result });
+            });
+        });
+        router.get("/create", (req, res) => {
+            console.log("Got a GET request for the User Create page.");
+            res.render("users/create", { "title": "Ninja" });
+        });
         return router;
     }
 }
--- src/config/routes/UserRoutes.ts
+++ src/config/routes/UserRoutes.ts
@@ -1,23 +1,29 @@
 import express = require("express");
-import UserController = require("./../../app/controllers/UserController");
+import UserRepository = require("./../../app/repositories/UserRepository");
 
 let router = express.Router();
 
 class UserRoutes {
-    private _userController: UserController;
+    private _userRepository: UserRepository;
 
     constructor() {
-        this._userController = new UserController();
+        this._userRepository = new UserRepository();
     }
 
     get routes() {
-        let controller = this._userController;
+        let repository = this._userRepository;
 
-        router.delete("/:_id", controller.delete);
-        router.get("/", controller.retrieve);
-        router.get("/:_id", controller.findById);
-        router.post("/", controller.create);
-        router.put("/:_id", controller.update);
+        router.get("/", (req, res) => {
+            console.log("Got a GET request for the Users page.");
+            repository.retrieve((error, result) => {
+                res.render("users/list", { "title": "All My Ninjas", "users": result });
+            });
+        });
+
+        router.get("/create", (req, res) => {
+            console.log("Got a GET request for the User Create page.");
+            res.render("users/create", { "title": "Ninja" });
+        });
 
         return router;
     }
